0646ec059a08fe8c6cc8f0eb5c1216d5ab55c787
Quinto <amandaqt@38f9d3ad93a1.ant.amazon.com>
2020-03-11 11:25:18 -0300

    Adding auth

--- app.py
+++ app.py
@@ -5,26 +5,30 @@ import requests
 
 app = Flask(__name__)
 url = 'http://api.olhovivo.sptrans.com.br/v2.1'
-apikey = 'e10365af8aed9e34e765e828fe573e6d1961c02fe2a4d3fb7e55e1fcxxxxxxx'
+apikey = 'e10365af8aed9e34e765e828fe573e6d1961c02fe2a4d3fb7e55e1f'
 session = requests.session()
 stop_id = 340015329
 bus_id = 8000
-head = {'Authorization': 'token {}'.format(apikey)}
-
-@app.route('/home')
 
 def auth():
-    response = session.post(url + '/Login/Autenticar?token={}'.format(apikey), timeout=3)
-    print(response)
 
+    response = requests.post(url + '/Login/Autenticar?token={}'.format(apikey), timeout=3)
+    x = response.cookies
+    cookie = requests.utils.dict_from_cookiejar(x)
+    linha(cookie)
+
+
+@app.route('/Linha/Buscar?termosBusca=<string:cod_>', methods=['GET'])
+def linha(cookie):
+    response = requests.get(url + '/Linha/Buscar?termosBusca={}'.format(bus_id), cookies=cookie, timeout=3)
+    result = response.cookies
+    
+@app.route('/home', methods=['GET'])
 def home():
     return redirect("http://mandalvesq.github.io")
 
-
-
-
-#@app.route('/Previsao?codigoParada=<string:cod_>&codigoLinha=<string:cod_>', methods=['GET'])
-def get_previsao(stop_id, bus_id):
+@app.route('/Previsao?codigoParada=<string:cod_>', methods=['GET'])
+def get_previsao():
     result = session.get(url + '/Previsao?codigoParada={}&codigoLinha={}'.format(stop_id, bus_id))
     print(result)
     return result
